1f8a6cbac9b961c398b0183d8866ab0d83df1834
Bubi Blomer <bubiblomer@gmail.com>
2023-01-04 17:29:02 -0400

    Prueba 1

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,7 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function hello(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'Bambang' });
+export default async function hello(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    console.log(req.body);
+
+    // parse the request body
+    const body = JSON.parse(req.body);
+
+    // get the message text from the request body
+    // const message = body.message;
+
+    // send a response
+    res.status(200).json({ body });
+  } else {
+    // handle other HTTP methods
+    res.status(405).end();
+  }
 }
