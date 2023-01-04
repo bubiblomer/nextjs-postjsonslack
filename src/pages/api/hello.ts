5569172f4fe7170e92417601e64d0515f8de6966
Bubi Blomer <bubiblomer@gmail.com>
2023-01-04 18:01:36 -0400

    json tst 5

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,14 +3,17 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function hello(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
+    console.log(req.body);
     console.log(req.body.paymentIntent.id);
 
     // parse the request body
+    const body = JSON.parse(req.body);
+
     // get the message text from the request body
     //const message = body.message;
 
     // send a response
-    res.status(200);
+    res.status(200).json({ body });
   } else {
     // handle other HTTP methods
     res.status(405).end();
