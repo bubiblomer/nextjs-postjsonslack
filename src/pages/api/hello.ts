14a1542b05f8f3d65362c46f3818b08f0e737c5c
Bubi Blomer <bubiblomer@gmail.com>
2023-01-04 17:57:51 -0400

    tets json final

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,12 +3,9 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function hello(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    console.log(req.body);
     console.log(req.body.paymentIntent.id);
 
     // parse the request body
-    const body = JSON.parse(req.body);
-
     // get the message text from the request body
     //const message = body.message;
 
