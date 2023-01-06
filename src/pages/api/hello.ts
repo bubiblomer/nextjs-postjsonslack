5ae8dc2cf60faec308bffb1b81d6f71086ad24a1
Bubi Blomer <bubiblomer@gmail.com>
2023-01-05 23:15:46 -0400

    test this

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -4,6 +4,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function hello(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { name } = req.body;
+
+    const testthis = JSON.parse(req.body);
+    console.log(testthis);
+
     console.log(name);
     console.log(req.body.paymentIntent.id);
 
